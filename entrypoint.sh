492c820468569378a32162d6d04e72ff8228ea1e
yutian81 <yutian88881@hotmail.com>
2025-10-27 01:30:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# 定义cron任务所需的环境变量
-CRON_ENV_FILE="/etc/cron_env.sh"
+# 定义文件路径
+CRON_ENV_FILE="app/cron_env.sh"
+CRONTAB_FILE="/etc/crontabs/root"
+BACKUP_SCRIPT="/app/data/komari_bak.sh"
 
 echo "#!/usr/bin/env bash" > "$CRON_ENV_FILE"
 echo "export GH_BACKUP_USER=\"$GH_BACKUP_USER\"" >> "$CRON_ENV_FILE"
@@ -10,12 +12,15 @@ echo "export GH_PAT=\"$GH_PAT\"" >> "$CRON_ENV_FILE"
 echo "export GH_EMAIL=\"$GH_EMAIL\"" >> "$CRON_ENV_FILE"
 chmod +x "$CRON_ENV_FILE"
 
-# 启动cron服务
+# UTC 20:00 (北京时间 04:00) 启动备份任务
+echo "0 20 * * * . $CRON_ENV_FILE && $BACKUP_SCRIPT bak" > "$CRONTAB_FILE"
+
+# 启动 Cron 服务 (在后台运行)
 echo "正在启动 Cron 服务..."
 /usr/sbin/crond -f & 
 CRON_PID=$!
 
-# 启动komari面板
+# 启动komari面板 (作为主进程)
 echo "正在启动 Komari 面板..."
 exec "$@"
 
